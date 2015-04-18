fb347f10a0e47448491e67bb648f259eb272cc14
bastianlang@t-online.de <bastianlang@t-online.de>
2015-04-18 22:32:21 +0200

    fixed resource paths

--- assignments/src/org/brsu/assignments/assignment1/Application.java
+++ assignments/src/org/brsu/assignments/assignment1/Application.java
@@ -8,31 +8,28 @@ public class Application {
   public static void main(String[] args) throws IOException {
     MapReader mapReader = new MapReader();
     List<List<String>> map1 = mapReader
-        .readMapFromFile("src/resources/maps/map1.txt");
+        .readMapFromFile("resources/maps/map1.txt");
     List<List<String>> map2 = mapReader
-        .readMapFromFile("src/resources/maps/map2.txt");
+        .readMapFromFile("resources/maps/map2.txt");
     List<List<String>> map3 = mapReader
-        .readMapFromFile("src/resources/maps/map3.txt");
+        .readMapFromFile("resources/maps/map3.txt");
 
     Agent agent = new Agent();
-    agent.exploreMap(map1, "src/resources/results/map1_bfs",
-        Strategy.BREADTH_FIRST);
+    agent
+        .exploreMap(map1, "resources/results/map1_bfs", Strategy.BREADTH_FIRST);
     agent = new Agent();
-    agent.exploreMap(map2, "src/resources/results/map2_bfs",
-        Strategy.BREADTH_FIRST);
+    agent
+        .exploreMap(map2, "resources/results/map2_bfs", Strategy.BREADTH_FIRST);
     agent = new Agent();
-    agent.exploreMap(map3, "src/resources/results/map3_bfs",
-        Strategy.BREADTH_FIRST);
+    agent
+        .exploreMap(map3, "resources/results/map3_bfs", Strategy.BREADTH_FIRST);
 
     agent = new Agent();
-    agent.exploreMap(map1, "src/resources/results/map1_dfs",
-        Strategy.DEPTH_FIRST);
+    agent.exploreMap(map1, "resources/results/map1_dfs", Strategy.DEPTH_FIRST);
     agent = new Agent();
-    agent.exploreMap(map2, "src/resources/results/map2_dfs",
-        Strategy.DEPTH_FIRST);
+    agent.exploreMap(map2, "resources/results/map2_dfs", Strategy.DEPTH_FIRST);
     agent = new Agent();
-    agent.exploreMap(map3, "src/resources/results/map3_dfs",
-        Strategy.DEPTH_FIRST);
+    agent.exploreMap(map3, "resources/results/map3_dfs", Strategy.DEPTH_FIRST);
 
   }
 }
